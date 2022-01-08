79d2f8159587ce8d889071046a2b8b43fdb0a4ac
TigerEngine124 <me2124.tech@gmail.com>
2022-01-07 22:32:45 -0300

    NextAuth.js com Provider Credentials changed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,18 +4,11 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
   providers: [
     Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
+      credentials: {},
+      async authorize() {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
+        const user = null;
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
