835e40a548863a56aec19ad0c2eac5897b7e5327
TigerEngine124 <me2124.tech@gmail.com>
2022-01-11 23:22:47 -0300

    Templates

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
 import { StrapiPost } from '../components/FormPost';
 import { PrivateComponent } from '../components/PrivateComponent';
-import { UpdatePostTemplate } from '../components/templates/UpdatePost';
+import { UpdatePostTemplate } from '../templates/UpdatePost';
 import { gqlClient } from '../graphql/client';
 import { GQL_QUERY_GET_POST } from '../graphql/queries/post';
 import { serverSideRedirect } from '../utils/server-side-redirect';
@@ -18,3 +18,33 @@ export default function PostPage({ post }: PostPageProps) {
     </PrivateComponent>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+  const { id } = ctx.params;
+
+  if (!session) {
+    return serverSideRedirect(ctx);
+  }
+
+  try {
+    const { post } = await gqlClient.request(
+      GQL_QUERY_GET_POST,
+      {
+        id,
+      },
+      {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    );
+
+    return {
+      props: {
+        session,
+        post,
+      },
+    };
+  } catch (e) {
+    return serverSideRedirect(ctx);
+  }
+};
